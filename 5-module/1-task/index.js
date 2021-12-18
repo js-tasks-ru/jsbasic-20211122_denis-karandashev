5ca055cb126eb7f11eef1f0225dc793dc0aacf1f
dankarandashev <dan.karandashev@gmail.com>
2021-12-18 11:46:22 +0300

    5-task-3

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,4 +1,4 @@
 function hideSelf() {
-  const elem = document.querySelector('.hide-self-button')
+  const elem = document.querySelector('.hide-self-button');
   elem.addEventListener('click', () => elem.hidden = true);
 }
